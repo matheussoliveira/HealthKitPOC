56dd31c18c456f2ccb2a8047c5ab99823cfd1aa4
Joao Victor Flores <j199818@dac.unicamp.br>
2020-11-06 15:08:20 -0300

    TestNotification

--- HealthKitPOC/HomeTableViewController.swift
+++ HealthKitPOC/HomeTableViewController.swift
@@ -6,15 +6,72 @@
 //
 
 import UIKit
+import UserNotifications
+import WatchConnectivity
 
-class HomeTableViewController: UITableViewController {
-    
-    
+class HomeTableViewController: UITableViewController, WCSessionDelegate, UNUserNotificationCenterDelegate {
+
+	var wcSession : WCSession! = nil
+
+	func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+
+	}
+
+	func sessionDidBecomeInactive(_ session: WCSession) {
+
+	}
+
+	func sessionDidDeactivate(_ session: WCSession) {
+
+	}
+
+	@IBAction func notificationAction(_ sender: Any) {
+		print("notificação mandada")
+		// Create Notification Content
+		let notificationContent = UNMutableNotificationContent()
+
+		// Configure Notification Content
+		notificationContent.title = "TITLE"
+		notificationContent.subtitle = "Subtitle"
+		notificationContent.body = "Body"
+		notificationContent.categoryIdentifier = "app.likedislike.ios10"
+
+		// Add Trigger
+		let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 1.0, repeats: false)
+
+		// Create Notification Request
+		let request = UNNotificationRequest(identifier: "app.likedislike.ios10",
+											content: notificationContent, trigger: notificationTrigger)
+		UNUserNotificationCenter.current().add(request, withCompletionHandler: { (error) in
+			if let error = error {
+				print("Error \(error)")
+				// Something went wrong
+			}
+		})
+	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
         requestHKAutorization()
+
+		wcSession = WCSession.default
+		wcSession.delegate = self
+		wcSession.activate()
+
+		UNUserNotificationCenter.current().delegate = self
+
+		authorizeNotification()
     }
+
+	func authorizeNotification() {
+		UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (success, error) in
+			if let error = error {
+				print("Error:- \(error)")
+			} else if success == true {
+				print("Permission Granted")
+			}
+		}
+	}
     
     private func requestHKAutorization() {
         
@@ -37,6 +94,13 @@ class HomeTableViewController: UITableViewController {
         }
     }
 
+	func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+		completionHandler([.alert])
+	}
 
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 }
 
