0c5653e74abdb2cabe8c412dd4066de2fcaece28
Joao Victor Flores <j199818@dac.unicamp.br>
2020-11-13 15:38:02 -0300

    refactory

--- WatchHealthKitPOC Extension/Controller/InterfaceController.swift
+++ WatchHealthKitPOC Extension/Controller/InterfaceController.swift
@@ -30,14 +30,11 @@ class InterfaceController: WKInterfaceController {
 	let healthStore = HKHealthStore()
 	var session: HKWorkoutSession!
 	var builder: HKLiveWorkoutBuilder!
-
 	var heartrate: Double = 0
 	var activeCalories: Double = 0
 	var distance: Double = 0
 	var elapsedSeconds: Int = 0
-
 	var running: Bool = false
-
 	var start: Date = Date()
 	var cancellable: Cancellable?
 	var accumulatedTime: Int = 0
@@ -47,7 +44,6 @@ class InterfaceController: WKInterfaceController {
 		super.awake(withContext: context)
 
 		NotificationManager().setupNotifications()
-		NotificationManager().setReminderNotification()
 
 		requestAuthorization()
 		startWorkout()
@@ -100,4 +96,114 @@ class InterfaceController: WKInterfaceController {
 		let seconds = String(format: "%02d", (seconds % 3600) % 60)
 		return hours + ":" + minuts + ":" + seconds
 	}
+}
+
+extension InterfaceController: HKWorkoutSessionDelegate, HKLiveWorkoutBuilderDelegate {
+	func workoutConfiguration() -> HKWorkoutConfiguration {
+		let configuration = HKWorkoutConfiguration()
+		configuration.activityType = .running
+		configuration.locationType = .outdoor
+
+		return configuration
+	}
+
+	//	MARK: - Workout
+	func startWorkout() {
+		setUpTimer()
+
+		self.running = true
+
+		do {
+			session = try HKWorkoutSession(healthStore: healthStore, configuration: workoutConfiguration())
+			builder = session.associatedWorkoutBuilder()
+		} catch {
+			return
+		}
+
+		session.delegate = self
+		builder.delegate = self
+
+		builder.dataSource = HKLiveWorkoutDataSource(healthStore: healthStore,
+													 workoutConfiguration: workoutConfiguration())
+
+		session.startActivity(with: Date())
+		builder.beginCollection(withStart: Date()) { (success, error) in
+			print(error.debugDescription)
+		}
+	}
+
+	func endWorkout() {
+		session.end()
+		cancellable?.cancel()
+	}
+
+	func resetWorkout() {
+		DispatchQueue.main.async {
+			self.elapsedSeconds = 0
+			self.activeCalories = 0
+			self.heartrate = 0
+			self.distance = 0
+		}
+	}
+
+	// MARK: - Update the UI
+	func updateForStatistics(_ statistics: HKStatistics?) {
+		guard let statistics = statistics else { return }
+
+		DispatchQueue.main.async {
+			switch statistics.quantityType {
+				case HKQuantityType.quantityType(forIdentifier: .heartRate):
+					/// - Tag: SetLabel
+					let heartRateUnit = HKUnit.count().unitDivided(by: HKUnit.minute())
+					let value = statistics.mostRecentQuantity()?.doubleValue(for: heartRateUnit)
+					let roundedValue = Double( round( 1 * value! ) / 1 )
+					self.heartrate = roundedValue
+				case HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned):
+					let energyUnit = HKUnit.kilocalorie()
+					let value = statistics.sumQuantity()?.doubleValue(for: energyUnit)
+					self.activeCalories = Double( round( 1 * value! ) / 1 )
+					return
+				case HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning):
+					let meterUnit = HKUnit.meter()
+					let value = statistics.sumQuantity()?.doubleValue(for: meterUnit)
+					let roundedValue = Double( round( 1 * value! ) / 1 )
+					self.distance = roundedValue
+					return
+				default:
+					return
+			}
+		}
+	}
+
+	func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState,
+						from fromState: HKWorkoutSessionState, date: Date) {
+		if toState == .ended {
+			builder.endCollection(withEnd: Date()) { (success, error) in
+				self.builder.finishWorkout { (workout, error) in
+					self.resetWorkout()
+				}
+			}
+		}
+	}
+
+	func workoutBuilder(_ workoutBuilder: HKLiveWorkoutBuilder, didCollectDataOf collectedTypes: Set<HKSampleType>) {
+		for type in collectedTypes {
+			guard let quantityType = type as? HKQuantityType else {
+				return
+			}
+
+			let statistics = workoutBuilder.statistics(for: quantityType)
+
+			updateForStatistics(statistics)
+		}
+
+		heartrateLabel.setText("\(heartrate) bpm")
+		activeCaloriesLabel.setText("\(activeCalories) cal")
+		distanceLabel.setText("\(distance) m")
+		timerLabel.setText(secondsToHoursMinutesSeconds(seconds: elapsedSeconds))
+	}
+
+	func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) { }
+
+	func workoutBuilderDidCollectEvent(_ workoutBuilder: HKLiveWorkoutBuilder) { }
 }
