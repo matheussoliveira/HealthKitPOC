a6ff8ccae08f517e0f4f0da2fc31fd56194ccd43
Matheus de Souza Oliveira <m203407@dac.unicamp.br>
2020-11-16 16:08:24 -0300

    Changing pedometer to background thread

--- HealthKitPOC/PedometerViewController.swift
+++ HealthKitPOC/PedometerViewController.swift
@@ -13,6 +13,10 @@ class PedometerViewController: UIViewController {
     @IBOutlet weak var stepCounter: UILabel!
     
     let pedometer = CMPedometer()
+    var counter: Int = 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +25,28 @@ class PedometerViewController: UIViewController {
     }
     
     private func startPedometer() {
-        pedometer.startUpdates(from: Date()) { (data, error) in
-            DispatchQueue.main.async {
-                self.stepCounter.text = "\(data?.numberOfSteps ?? 0)"
+        DispatchQueue.background {
+            self.pedometer.startUpdates(from: Date()) { (data, error) in
+                self.counter = Int(truncating: data?.numberOfSteps ?? 0)
+                DispatchQueue.main.async {
+                    self.stepCounter.text = String(self.counter)
+                }
             }
         }
     }
+}
 
+extension DispatchQueue {
+
+    static func background(_ task: @escaping () -> ()) {
+        DispatchQueue.global(qos: .background).async {
+            task()
+        }
+    }
+
+    static func main(_ task: @escaping () -> ()) {
+        DispatchQueue.main.async {
+            task()
+        }
+    }
 }
